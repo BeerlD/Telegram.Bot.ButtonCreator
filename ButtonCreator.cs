c3e8a5e364e7558dede234eaed4fd8db52838486
BeerlD <bellkunmorenime@gmail.com>
2025-02-09 19:32:58 -0300

    Adicionar arquivos de projeto.

---
+++ ButtonCreator.cs
@@ -0,0 +1,26 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Telegram.Bot.ButtonCreator
+{
+    public class ButtonCreator
+    {
+        private List<List<InlineKeyboardButton>> Buttons = new List<List<InlineKeyboardButton>>();
+        private int lineIndex = 0;
+
+        public void CreateNewLine()
+        {
+            Buttons.Add(new List<InlineKeyboardButton>());
+            lineIndex += 1;
+        }
+
+        public void AddButton(string title, string id)
+        {
+            Buttons[lineIndex].Add(InlineKeyboardButton.WithCallbackData(title, id));
+        }
+
+        public InlineKeyboardMarkup GetLayout()
+        {
+            return new InlineKeyboardMarkup(Buttons);
+        }
+    }
+}
